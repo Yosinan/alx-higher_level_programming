5f757db44912b1c4b6fb83af7802b91688fca42e
Yoseph <ymymym4634@gmail.com>
2023-02-22 21:17:17 +0300

    MySQL

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in MySQL server.
-SELECT count(*) FROM first_table WHERE id = 98;
+SELECT count(*) FROM first_table WHERE id = 89;

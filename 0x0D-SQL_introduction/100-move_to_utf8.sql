e1875790a376938751a47dbe643b252933b89868
Yoseph <ymymym4634@gmail.com>
2023-02-22 22:35:24 +0300

    mySQL

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0 
-   CHARACTER SET utf8mb4 
+   CHARACTER SET utf8mb4
    COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
@@ -10,7 +10,7 @@ ALTER TABLE first_table
       COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-      CHANGE `name`
+      CHANGE name
       VARCHAR(256)
       CHARACTER SET utf8mb4
       COLLATE utf8mb4_unicode_ci;

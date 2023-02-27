6c8985f7b01df4b6c8cbbb377019c54662bd6684
Yos <99474058+Yosinan@users.noreply.github.com>
2023-02-27 19:12:02 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """ Return True if the object is an instance of a class
+    """Return True if the object is an instance of a class
     that inherited (directly or indirectly) from the
     specified class, otherwise False."""
     return (issubclass(type(obj), a_class) and type(obj) != a_class)

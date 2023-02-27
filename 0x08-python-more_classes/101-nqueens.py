419d6029ae70c3845c635ccd696304ce4586f99c
Yos <99474058+Yosinan@users.noreply.github.com>
2023-02-27 19:53:12 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-""" Solves the N-queens puzzle.
+"""Solves the N-queens puzzle.
 
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Example:
+    $ ./101-nqueens.py N
 
 N must be an integer greater than or equal to 4.
 

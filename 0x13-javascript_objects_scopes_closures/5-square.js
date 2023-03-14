abce936e0bf31e70dbfb3b3068e43707171df87d
Yosinan <ymymym4634@gmail.com>
2023-03-14 03:25:11 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,8 +2,8 @@
 // a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
-	constructor (size) {
-		super(size,size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
 module.exports = Square;

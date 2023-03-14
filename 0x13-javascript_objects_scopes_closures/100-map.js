ff41cb3235d6ed32c219ddc07cdc71b0e23bde39
Yosinan <ymymym4634@gmail.com>
2023-03-14 04:37:49 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
 // a script that imports an array and computes a new array
+
+const list = require('./100-data.js').list;
+console.log(list);
+
+const newList = list.map((y, indice) => (y * indice)
+);
+console.log(newList);

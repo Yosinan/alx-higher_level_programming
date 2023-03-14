27ee455421cb8cd0ba9a5596ea9ee5e9e3a6ec9e
Yosinan <ymymym4634@gmail.com>
2023-03-14 15:16:02 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // a script that concats 2 files
-
-const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+const fs = require('fs');
+const d1 = fs.readFileSync(process.argv[2], 'utf8');
+const d2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], d1 + d2);

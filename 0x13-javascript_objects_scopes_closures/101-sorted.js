1db45274e90fe80bcbc86ab6944aada26598e517
Yosinan <ymymym4634@gmail.com>
2023-03-14 15:07:36 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,3 +2,11 @@
 // a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
 
 const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+	     newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);

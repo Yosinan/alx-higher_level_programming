4fa40a727618d72ff71baf7d7c260da61aa29f05
Yosinan <ymymym4634@gmail.com>
2023-03-14 03:35:32 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a class Square that defines a square and inherits from Square of 5-square.js
 
-const Square = require('./5-square.js');
-class Square extends Square {
+const square = require('./5-square.js');
+class Square extends square {
   charPrint (c) {
     if (c === undefined) {
        c = 'X';

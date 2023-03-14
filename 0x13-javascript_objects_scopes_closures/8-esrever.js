a0cbf2a602cfc4cea69f7edd7c356110fd4526fb
Yosinan <ymymym4634@gmail.com>
2023-03-14 03:52:55 -0700

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
+#!/usr/bin/node
 // a function that returns the reversed version of a list
+
+exports.esrever = function (list) {
+  let j = list.length - 1;
+  const newList = [];
+  for (j; j >= 0; j--) {
+    newList.append(j[list]);
+  }
+};

e5faf8613276baf2cb509eb0e9667c15810a70c4
Yosinan <ymymym4634@gmial.com>
2023-03-16 09:00:31 -0700

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# a script that lists all states from the database hbtn_0e_0_usa
+
+import MySQLdb
+con = MySQLdb.connect(host='localhost', port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+cur = con.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+row_query = cur.fetchall()
+
+for row in row_query:
+    print(row)
+cur.close()
+con.close()

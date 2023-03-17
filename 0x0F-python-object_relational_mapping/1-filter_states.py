0c3d1ec5a46d744476e8774803ab897ac6dec9e4
Yosinan <ymymym4634@gmial.com>
2023-03-17 02:51:41 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 if __name__ == '__main__':
     # make a connection to the database
     con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                          passwd=argv[2], db=argv[3])
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.

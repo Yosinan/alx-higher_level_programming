486b919a78167464c96fab3695c3645b895575e8
Yosinan <ymymym4634@gmial.com>
2023-03-17 03:59:39 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# a script that lists all cities from the database hbtn_0e_0_usa
+'''
+a script that lists all cities from the database hbtn_0e_0_usa
+'''
 
 import MySQLdb
 from sys import argv

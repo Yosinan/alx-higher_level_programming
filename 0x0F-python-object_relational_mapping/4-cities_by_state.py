b8ed61cd53b4c887cfab420bb052cee9ba449343
Yosinan <ymymym4634@gmial.com>
2023-03-17 04:15:24 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         user=argv[1],
         passwd=argv[2],
         db=argv[3]
-        )
+    )
     cur = con.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
             INNER JOIN states ON cities.state_id = states.id\

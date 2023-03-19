e3f1d689fa43fbaebca0ad60e6d5ac5b234b1e5b
Yosinan <ymymym4634@gmial.com>
2023-03-19 03:52:12 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+module that creates a class 'city' based on Base
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """
+    City class that inherits from Base
+
+    Attributes:
+
+        id: Id city
+        name: Name of city
+        state_id: State id
+
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

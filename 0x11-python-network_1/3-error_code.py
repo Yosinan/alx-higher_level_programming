2d9bf27ca3275d0f34c22c06583a7cb47eaf903a
Yosinan <ymymym4634@gmial.com>
2023-03-31 10:19:39 -0700

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf8'))
-    except urllib.exceptions.HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))

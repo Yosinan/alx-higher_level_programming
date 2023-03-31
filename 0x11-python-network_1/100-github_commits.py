1c40f66666a4638302f8543fa988e3ee2feac138
Yosinan <ymymym4634@gmial.com>
2023-03-31 14:10:42 -0700

    0x11-python-network_1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,7 +2,7 @@
 # a pyhthon script that will list 10 commits (from the most recent to
 # oldest) of the repository “rails” by the user “rails”
 
-from sys import argv 
+from sys import argv
 import requests
 
 if __name__ == '__main__':
@@ -11,7 +11,10 @@ if __name__ == '__main__':
     owner_name = sys.argv[2]
 
     res = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(owner_name, repo_name))
+                       .format(owner_name, repo_name))
     js = res.json()
     for i in js[0:10]:
-        print("{}: {}".format(i.get('sha'), i.get('commit').get('author').get('name')))
+        print(
+            "{}: {}".format(
+                i.get('sha'),
+                i.get('commit').get('author').get('name')))

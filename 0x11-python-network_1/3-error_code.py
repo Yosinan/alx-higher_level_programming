232e38b0b8709d0a2dbf38d9a3024631bc4a4919
Yosinan <ymymym4634@gmial.com>
2023-03-31 10:22:15 -0700

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,16 +2,16 @@
 """
  Python script that takes in a URL, sends a request to the URL
  and manage HTTP Errors
- 
+
 """
 
 import sys
 import urllib.request
 
 if __name__ == '__main__':
-    
+
     req = urllib.request.Request(sys.argv[1])
-    
+
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf8'))

658d5f1bf1a2231e59403476ea16a37100b40db4
Yosinan <ymymym4634@gmial.com>
2023-03-31 11:12:04 -0700

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,20 +10,20 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    
+
     if len(sys.argv) > 1:
         d = {'q': sys.argv[1]}
     else:
         d = {'q': ""}
-    
+
     info = requests.post('http://0.0.0.0:5000/search_user', d)
-    
+
     try:
         json_file = info.json()
         if json_file:
             print("[{}] {}".format(json_file['id'], json_file['name']))
         else:
             print("No result")
-            
+
     except ValueError:
         print("Not a valid JSON")

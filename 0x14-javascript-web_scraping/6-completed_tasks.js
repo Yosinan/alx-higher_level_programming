a2ae60a04ff30350075ad181eba4378219723b71
Yosinan <ymymym4634@gmail.com>
2023-04-25 13:24:46 -0700

    JavaScript Web Scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id
+const link = process.argv[2];
+const request = require('request');
+
+request(link, (err, res, body) => {
+  if (err) console.log(err);
+  const totalCompletedTask = {};
+
+  for (let k = 1; k <= 10; k++) {
+    const totCompleted = JSON.parse(body).filter(todo => todo.completed === true).filter(todo => todo.userId === k).length;
+    while (totCompleted) {
+      totalCompletedTask[i] = totCompleted;
+      break;
+    }
+  }
+  console.log(totalCompletedTask);
+});

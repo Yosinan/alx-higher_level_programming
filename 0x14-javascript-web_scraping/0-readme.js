a6eb7fd790589c8686abe1015a7706f0ed471d61
Yosinan <ymymym4634@gmail.com>
2023-04-25 11:10:40 -0700

    JavaScript Web Scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
 // importing built-in modules
-const ReadFile = require('fs');
-const argv = require('process');
+const { readFile } = require('fs');
+const { argv } = require('process');
 const path = argv[2];
 
-ReadFile(path, 'utf8', function (err, data) {
+readFile(path, 'utf8', function (err, data) {
   if (err) console.log(err);
   else console.log(data);
 });
